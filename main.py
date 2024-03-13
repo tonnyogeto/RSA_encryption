4e0d8a29e47e5061019cfd34383f0cb77b5e3e7f
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-03-13 23:40:31 +0300

    debug

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import random
 import math
 
@@ -5,57 +6,54 @@ def is_prime(number):
     if number < 2:
         return False
     
-    for i in range(2, number// 2+1):
+    for i in range(2, number // 2 + 1):
         if number % i == 0:
             return False
         
     return True
 
-
 def generate_prime(min_value, max_value):
-    prime = random.randint(min_value,max_value)
+    prime = random.randint(min_value, max_value)
     while not is_prime(prime):
-        prime=random.randint(min_value, max_value)
+        prime = random.randint(min_value, max_value)
     return prime
 
-
 def mod_inverse(e, phi):
-    for d in range(3,phi):
-        if(d*e) % phi == 1:
+    for d in range(3, phi):
+        if (d * e) % phi == 1:
             return d
         
     raise ValueError("mod_inverse does not exist")
 
-p,q = generate_prime(1000,5000), generate_prime(1000,5000)
+p, q = generate_prime(1000, 5000), generate_prime(1000, 5000)
 
 while p == q:
-    q= generate_prime(1000, 5000)
+    q = generate_prime(1000, 5000)
 
-n =p*q
+n = p * q
+phi_n = (p - 1) * (q - 1)
 
-phi_n =(p-1) *(q-1)
+e = random.randint(3, phi_n - 1)
+while math.gcd(e, phi_n) != 1:
+    e = random.randint(3, phi_n - 1)
 
-e = random.randint(3,phi_n-1)
-while math.gcd(e, phi_n)!=1:
-    e=random.randint(3, phi_n-1)
+d = mod_inverse(e, phi_n)
 
-d=mod_inverse(e, phi_n)
-
-print("Public key: ",e)
-print("Private key: ",d)
-print("n: ",n)
-print("phi of n : ",phi_n)
+print("Public key: ", e)
+print("Private key: ", d)
+print("n: ", n)
+print("phi of n : ", phi_n)
 print("p", p)
 print("q", q)
 
-message="Hello world"
+message = input("Enter the message to be encrypted: ")
 
 message_encoded = [ord(ch) for ch in message]
-ciphertext=[pow(ch,e,n) for ch in message_encoded]
+ciphertext = [pow(ch, e, n) for ch in message_encoded]
 
-print(ciphertext)
+print("Ciphertext:", ciphertext)
 
-message_encoded=[pow(ch,d,n)for ch in ciphertext]
-message="".join(chr(ch) for ch in message_encoded)
+message_encoded = [pow(ch, d, n) for ch in ciphertext]
+decrypted_message = "".join(chr(ch) for ch in message_encoded)
 
-print(message)
+print("Decrypted message:", decrypted_message)
